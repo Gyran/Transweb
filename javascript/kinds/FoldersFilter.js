102daaf5b86115cafa679785e7f721a1c4990fd2
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2012-04-30 00:56:18 +0200

    folders sorted by name

--- javascript/kinds/FoldersFilter.js
+++ javascript/kinds/FoldersFilter.js
@@ -13,22 +13,32 @@ enyo.kind({
 		this.inherited( arguments );
 	},
 
-	addFolderFilter: function( torrent ) {
+	filterFolders: function( torrent ) {
 		var folder = torrent.getDownloadFolder();
 
 		if ( enyo.indexOf( folder, this.folders ) === -1 ) {
 			this.folders.push( folder );
-			this.createComponent({
-				kind: "FolderFilter",
-				folder: folder
-			});
-		}	
+		}
 	},
+
+	addFolderFilter: function ( folder ) {
+		this.createComponent({
+			kind: "FolderFilter",
+			folder: folder
+		});
+	},
+
 
 	torrentsUpdated: function( sender ) {
 		this.folders = [];
 		this.destroyClientControls( );
-		enyo.forEach( enyo.application.getTorrents( ), enyo.bind( this, "addFolderFilter" ), this );
+		enyo.forEach( enyo.application.getTorrents( ), enyo.bind( this, "filterFolders" ), this );
+		this.folders.sort( function ( a, b ){
+			an = a.toLowerCase();
+			bn = b.toLowerCase();
+			return ( an.localeCompare( bn ) );
+		});
+		enyo.forEach( this.folders, enyo.bind( this, "addFolderFilter" ), this );
 		this.render();
 	}
 });
