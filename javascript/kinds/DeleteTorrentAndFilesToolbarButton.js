24d21d05248fefe16d704d7d5c988b9cd5ff7705
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2012-05-21 11:29:03 +0200

    added confirm button to delete torrents and files toolbar button

--- javascript/kinds/DeleteTorrentAndFilesToolbarButton.js
+++ javascript/kinds/DeleteTorrentAndFilesToolbarButton.js
@@ -5,14 +5,18 @@ enyo.kind({
     content: "Delete torrent and files",
 
     tap: function( sender, event ) {
-        if( enyo.application.selectedTorrents.length <= 0 ) {
-            return;
-        }
 
-        this.bubble( "onStartLoading" );
-        new enyo.Ajax({url: "php/rpcconnection.php", method: "post" })
-        .response(this, "deleted")
-        .go( { method: "deleteTorrentsAndFiles", 'torrents[]': enyo.application.selectedTorrents } );
+        if ( confirm("Are you sure you want to delete the selected torrents and all of their files?") ) {
+
+            if ( enyo.application.selectedTorrents.length <= 0 ) {
+                return;
+            }
+
+            this.bubble( "onStartLoading" );
+            new enyo.Ajax({url: "php/rpcconnection.php", method: "post" })
+            .response(this, "deleted")
+            .go( { method: "deleteTorrentsAndFiles", 'torrents[]': enyo.application.selectedTorrents } );
+        }
     },
 
     deleted: function( sender, response ) {
